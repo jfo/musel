1b637cd0af355a650863cb8dfa586a1ddfc9d18e
urthbound <jeffowler@gmail.com>
2014-03-24 18:25:21 -0400

    added node tuner

---
+++ tuner.js
@@ -0,0 +1,25 @@
+require('locus');
+
+function derp(seed) {
+  acc = {};
+  seed = typeof seed !== 'undefined' ? seed : 440.0;
+
+  while (seed > 30) {
+    seed = seed / 2.0
+  }
+
+  var notes = ["A", "As","B","C", "Cs", "D", "Ds", "E", "F", "Fs", "G","Gs"];
+  var notesindex = 0
+
+  while (seed < 20000.00) {
+    acc[notes[(notesindex % 12)] + (Math.floor(notesindex/12)) ] = seed;
+    seed = seed * Math.pow(2.0, (1.0/12.0));
+    notesindex += 1
+ }
+
+ return acc
+}
+
+var stuff = derp();
+console.log(stuff)
+eval(locus)
