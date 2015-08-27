9ef80d83b28efaa95423801f27935f984d602602
Jeff Fowler <jeffowler@gmail.com>
2015-08-27 18:57:03 -0400

    jdsifojsd

---
+++ ugh1.rb
@@ -0,0 +1,23 @@
+include Math
+def musicsin(freq, x)
+    sin((2 * freq) * PI * x)
+end
+
+# p musicsin(440, 1000)
+
+def hextodec(n)
+    # eval(("0x" + n) + 0)
+    "0x" + n
+end
+
+
+hextodec("208")
+p 0x0208 + 0
+# p 0xfd0f + 0
+# p 0xe717 + 0
+# p 0xb91f + 0
+# p 0x6c27 + 0
+# p 0xf62e + 0
+# p 0x5236 + 0
+# p 0x773d + 0
+# p 0x5e44 + 0
